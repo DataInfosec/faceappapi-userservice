ba5e7a0ae57377ce6938faefa738547920264720
alactic <okaforechezona1992@gmail.com>
2020-04-01 17:12:04 +0100

    adding unit test to userservice

--- service/service_test.go
+++ service/service_test.go
@@ -16,12 +16,16 @@ type Repository struct {
 // user details of the person signing to the system
 var idStr = "5e1f799738794b700e14cdc9"
 var userId, _ = primitive.ObjectIDFromHex(idStr)
-var user = entity.User{Type: "admin",
-	Firstname:       "elvis",
-	Lastname:        "eche",
-	Email:           "okaforechezona@gmail.com",
-	Password:        "password",
-	Confirmpassword: "password",
+var user = entity.User{
+	Type:         "admin",
+	Firstname:    "elvis",
+	Lastname:     "eche",
+	Email:        "okaforechezona@gmail.com",
+	OfficeId:     "office123",
+	Image:        "",
+	Active:       "active",
+	Company:      "5e7de42bdc3f7707ff61be7e",
+	SecretAnswer: "ffgg",
 }
 
 func TestCreate(t *testing.T) {
@@ -51,51 +55,63 @@ func TestUpdate(t *testing.T) {
 func TestInvalidType(t *testing.T) {
 	var result entity.User
 	var err error
-	var body = entity.User{
-		Type:            "",
-		Firstname:       "elvis",
-		Lastname:        "eche",
-		Email:           "okaforechezona@gmail.com",
-		Password:        "password",
-		Confirmpassword: "password",
+	var user = entity.User{
+		Type:         "",
+		Firstname:    "elvis",
+		Lastname:     "eche",
+		Email:        "okaforechezona@gmail.com",
+		OfficeId:     "office123",
+		Image:        "",
+		Active:       "active",
+		Company:      "5e7de42bdc3f7707ff61be7e",
+		SecretAnswer: "ffgg",
 	}
-	result, err = CreateValidation(body)
+	result, err = CreateValidation(user)
 
 	assert.NotNil(t, result)
 	assert.NotNil(t, err)
 	assert.Equal(t, err.Error(), "Type is required")
 }
 
+func FakeFetch(user entity.User) string {
+	return "mocked Fetch"
+}
+
 func TestInvalidEmail(t *testing.T) {
 	var result entity.User
 	var err error
-	var body = entity.User{
-		Type:            "admin",
-		Firstname:       "elvis",
-		Lastname:        "eche",
-		Email:           "",
-		Password:        "password",
-		Confirmpassword: "password",
+	var user = entity.User{
+		Type:         "admin",
+		Firstname:    "elvis",
+		Lastname:     "eche",
+		Email:        "",
+		OfficeId:     "office123",
+		Image:        "",
+		Active:       "active",
+		Company:      "5e7de42bdc3f7707ff61be7e",
+		SecretAnswer: "ffgg",
 	}
-	result, err = CreateValidation(body)
+	result, err = CreateValidation(user)
 
 	assert.NotNil(t, result)
-	assert.NotNil(t, err)
 	assert.Equal(t, err.Error(), "Valid Email is required")
 }
 
 func TestInvalidFirstname(t *testing.T) {
 	var result entity.User
 	var err error
-	var body = entity.User{
-		Type:            "admin",
-		Firstname:       "",
-		Lastname:        "eche",
-		Email:           "okaforechezona@gmail.com",
-		Password:        "password",
-		Confirmpassword: "password",
+	var user = entity.User{
+		Type:         "admin",
+		Firstname:    "",
+		Lastname:     "eche",
+		Email:        "okaforechezona@gmail.com",
+		OfficeId:     "office123",
+		Image:        "",
+		Active:       "active",
+		Company:      "5e7de42bdc3f7707ff61be7e",
+		SecretAnswer: "ffgg",
 	}
-	result, err = CreateValidation(body)
+	result, err = CreateValidation(user)
 
 	assert.NotNil(t, result)
 	assert.NotNil(t, err)
@@ -106,12 +122,15 @@ func TestInvalidLastname(t *testing.T) {
 	var result entity.User
 	var err error
 	var body = entity.User{
-		Type:            "admin",
-		Firstname:       "Elvis",
-		Lastname:        "",
-		Email:           "okaforechezona@gmail.com",
-		Password:        "password",
-		Confirmpassword: "password",
+		Type:         "admin",
+		Firstname:    "elvis",
+		Lastname:     "",
+		Email:        "okaforechezona@gmail.com",
+		OfficeId:     "office123",
+		Image:        "",
+		Active:       "active",
+		Company:      "5e7de42bdc3f7707ff61be7e",
+		SecretAnswer: "ffgg",
 	}
 	result, err = CreateValidation(body)
 
@@ -120,52 +139,19 @@ func TestInvalidLastname(t *testing.T) {
 	assert.Equal(t, err.Error(), "Last Name is required")
 }
 
-func TestInvalidPassword(t *testing.T) {
-	var result entity.User
-	var err error
-	var body = entity.User{
-		Type:            "admin",
-		Firstname:       "Elvis",
-		Lastname:        "eche",
-		Email:           "okaforechezona@gmail.com",
-		Password:        "",
-		Confirmpassword: "password",
-	}
-	result, err = CreateValidation(body)
-
-	assert.NotNil(t, result)
-	assert.NotNil(t, err)
-	assert.Equal(t, err.Error(), "Password is required")
-}
-
-func TestInvalidConfirmpassword(t *testing.T) {
-	var result entity.User
-	var err error
-	var body = entity.User{
-		Type:            "admin",
-		Firstname:       "Elvis",
-		Lastname:        "eche",
-		Email:           "okaforechezona@gmail.com",
-		Password:        "password",
-		Confirmpassword: "",
-	}
-	result, err = CreateValidation(body)
-
-	assert.NotNil(t, result)
-	assert.NotNil(t, err)
-	assert.Equal(t, err.Error(), "Confirm password is required")
-}
-
 func TestValidUserInput(t *testing.T) {
 	var result entity.User
 	var err error
 	var body = entity.User{
-		Type:            "admin",
-		Firstname:       "Elvis",
-		Lastname:        "eche",
-		Email:           "okaforechezona@gmail.com",
-		Password:        "password",
-		Confirmpassword: "password",
+		Type:         "admin",
+		Firstname:    "elvis",
+		Lastname:     "eche",
+		Email:        "okaforechezona@gmail.com",
+		OfficeId:     "office123",
+		Image:        "",
+		Active:       "active",
+		Company:      "5e7de42bdc3f7707ff61be7e",
+		SecretAnswer: "ffgg",
 	}
 	result, err = CreateValidation(body)
 
@@ -210,6 +196,19 @@ func TestFindById(t *testing.T) {
 
 	testService := UserService()
 	result, err = testService.FindById(idStr)
+
+	assert.NotNil(t, result)
+	assert.NotNil(t, err)
+}
+
+func TestUpdateImageInvalid(t *testing.T) {
+	var result string
+	var err error
+
+	var body = entity.UpdateImage{
+		Image: "",
+	}
+	result, err = UpdateImageValidation(body)
 
 	assert.NotNil(t, result)
 	assert.NotNil(t, err)
